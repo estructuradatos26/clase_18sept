81e986ac7a4af95c76743823ae44ed60b9291b54
llewelyn <inthecloud275@gmail.com>
2026-09-18 14:06:07 -0600

    Definiendo arreglos de edades

--- mx/edu/utng/RepasoArreglos.java
+++ mx/edu/utng/RepasoArreglos.java
@@ -4,5 +4,14 @@ public class RepasoArreglos{
 	public static void main(String[] args){
 		System.out.println("Manejando Arreglos");
 
+		String[] nombre = new String[10];
+		int edades[]= {20,21,22,23};
+
+		System.out.println("Tamaño "+nombre.length);
+		System.out.println("Edades "+edades.length);
+
+		//Indices 
+		nombre[0] = "Juan Alberto";
+		nombre[11] = "Juan Felipe";
 	}
 }
